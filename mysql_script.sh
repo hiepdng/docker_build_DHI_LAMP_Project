a031314098555d59721978d46d579b30bc3fd08d
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-07-19 17:16:38 -0700

    Add script to generate CA and certificates using OpenSSL
    This script generates a Certificate Authority (CA), server certificate, and client certificate using OpenSSL commands.

---
+++ mysql_script.sh
@@ -0,0 +1,22 @@
+## Create the Certificate Authority (CA): 
+# Generate CA Private Key
+openssl genrsa 2048 > ca-key.pem
+
+# Generate CA Certificate
+openssl req -new -x509 -nodes -days 3650 -key ca-key.pem -out ca.pem -subj '/C=US/ST=state/L=city/O=Organization/OU=Department/CN=server.example.com/emailAddress=you@example.com'
+
+
+### Create the Server Certificate:
+# Generate Server Private Key
+openssl req -newkey rsa:2048 -nodes -days 3650 -keyout server-key.pem -out server-req.pem -subj '/C=US/ST=state/L=city/O=Organization/OU=Department/CN=server.example.com/emailAddress=you@example.com'
+
+# Sign the Server Certificate with the CA
+openssl x509 -req -days 3650 -in server-req.pem -CA ca.pem -CAkey ca-key.pem -set_serial 01 -out server-cert.pem
+
+
+### Create the Client Certificate:
+# Generate Client Private Key
+openssl req -newkey rsa:2048 -days 3650 -nodes -keyout client-key.pem -out client-req.pem -subj '/C=US/ST=State/L=City/O=Organization/OU=Department/CN=client.example.com/emailAddress=you@example.com'
+
+# Sign the Client Certificate with the CA
+openssl x509 -req -days 3650 -in client-req.pem -CA ca.pem -CAkey ca-key.pem -set_serial 01 -out client-cert.pem
