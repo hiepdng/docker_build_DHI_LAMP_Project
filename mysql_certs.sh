7157e03ef7bbc5063606e187032eef6a70bbbd40
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-07-20 17:36:01 -0700

    Refactor certificate generation script for new directory structure

--- mysql_certs.sh
+++ mysql_certs.sh
@@ -3,31 +3,46 @@
 ### Check if openssl exists
 which openssl || { echo 'openssl not found' ; echo Exit...; }
 
-### Create mysql_certs directory
-if [ ! -d "mysql_certs" ]; then
+### Create certs/httpd directory
+if [ ! -d "certs/httpd" ]; then
     echo "Create mysql_certs directory."
-    mkdir mysql_certs && chown 65532:65532 mysql_certs
+    mkdir -p certs/httpd
 fi
 
+### Create certs/mysql directory
+if [ ! -d "certs/mysql" ]; then
+    echo "Create mysql_certs directory."
+    mkdir -p certs/mysql
+fi
+
+
+### For HTTPD
+# Create a self-signed SSL Certificate for testing purposes:
+RUN openssl genrsa -des3 -passout pass:YourPasswordHere -out /certs/httpd/server.key.secure
+RUN openssl rsa -in /certs/httpd/server.key.secure -passin pass:YourPasswordHere -out /certs/httpd/server.key # decrypted server.key, used for auto start web withour password
+RUN openssl req -new -x509 -nodes -sha1 -days 365 -key /certs/httpd/server.key -out /certs/httpd/server.crt -extensions usr_cert  -subj "/C=US/ST=state/L=City/O=Organization/OU=Department/CN=server.example.com/emailAddress=you@example.com"
+
+
+### For MySQL
 ## Create the Certificate Authority (CA): 
 # Generate CA Private Key
 openssl genrsa 2048 > mysql_certs/ca-key.pem
 
 # Generate CA Certificate
-openssl req -new -x509 -nodes -days 3650 -key mysql_certs/ca-key.pem -out mysql_certs/ca.pem -subj '/C=US/ST=state/L=city/O=Organization/OU=Department/CN=server.example.com/emailAddress=you@example.com'
+openssl req -new -x509 -nodes -days 3650 -key certs/mysql/ca-key.pem -out certs/mysql/ca.pem -subj '/C=US/ST=state/L=city/O=Organization/OU=Department/CN=server.example.com/emailAddress=you@example.com'
 
 
 ### Create the Server Certificate:
 # Generate Server Private Key
-openssl req -newkey rsa:2048 -nodes -days 3650 -keyout mysql_certs/server-key.pem -out mysql_certs/server-req.pem -subj '/C=US/ST=state/L=city/O=Organization/OU=Department/CN=server.example.com/emailAddress=you@example.com'
+openssl req -newkey rsa:2048 -nodes -days 3650 -keyout certs/mysql/server-key.pem -out certs/mysql/server-req.pem -subj '/C=US/ST=state/L=city/O=Organization/OU=Department/CN=server.example.com/emailAddress=you@example.com'
 
 # Sign the Server Certificate with the CA
-openssl x509 -req -days 3650 -in mysql_certs/server-req.pem -CA mysql_certs/ca.pem -CAkey mysql_certs/ca-key.pem -set_serial 01 -out mysql_certs/server-cert.pem
+openssl x509 -req -days 3650 -in certs/mysql/server-req.pem -CA certs/mysql/ca.pem -CAkey certs/mysql/ca-key.pem -set_serial 01 -out certs/mysql/server-cert.pem
 
 
 ### Create the Client Certificate:
 # Generate Client Private Key
-openssl req -newkey rsa:2048 -days 3650 -nodes -keyout mysql_certs/client-key.pem -out mysql_certs/client-req.pem -subj '/C=US/ST=State/L=City/O=Organization/OU=Department/CN=client.example.com/emailAddress=you@example.com'
+openssl req -newkey rsa:2048 -days 3650 -nodes -keyout certs/mysql/client-key.pem -out certs/mysql/client-req.pem -subj '/C=US/ST=State/L=City/O=Organization/OU=Department/CN=client.example.com/emailAddress=you@example.com'
 
 # Sign the Client Certificate with the CA
-openssl x509 -req -days 3650 -in mysql_certs/client-req.pem -CA mysql_certs/ca.pem -CAkey mysql_certs/ca-key.pem -set_serial 01 -out mysql_certs/client-cert.pem
+openssl x509 -req -days 3650 -in certs/mysql/client-req.pem -CA certs/mysql/ca.pem -CAkey certs/mysql/ca-key.pem -set_serial 01 -out certs/mysql/client-cert.pem
