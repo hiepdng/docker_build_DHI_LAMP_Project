3e32da62f83159dc0320f19523709d44216e1737
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-07-27 14:18:37 -0700

    Add files via upload

---
+++ cleanup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+# This script will stop/delete all containers
+# and remove all images and network.
+#---------------------------------------------
+
+set -o allexport
+source .env
+set +o allexport
+
+# Stop and remove containers
+docker stop lamp-httpd lamp-mysql lamp-php-fpm
+docker rm lamp-httpd lamp-mysql lamp-php-fpm
+
+# Remove images
+docker rmi "${PROJECT_NAME}-${HTTPDVERSION}"
+docker rmi "${PROJECT_NAME}-${MYSQLVERSION}"
+docker rmi "${PROJECT_NAME}-${PHPVERSION}"
+
+# Remove docker network
+docker network ls --filter name=lamp_network -q |xargs -r docker network rm
+
+
+
