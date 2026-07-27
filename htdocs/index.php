64a8956bece5c3ee5a45c1c22680c0aa7a8097a8
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-07-27 09:57:30 -0700

    Add files via upload

---
+++ htdocs/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+
+<h3>Testing</h3>
+<ul>
+  <li><a href="module_list.php">List of php modules</a></li>
+  <li><a href="upload.php">Files Upload</a></li>
+  <li><a href="database.php">Database Insert and Display</a></li>
+</ul>
+</body>
+</html>
+
+<h3>Php info</h3>
+<?php
+phpinfo();
+?>
