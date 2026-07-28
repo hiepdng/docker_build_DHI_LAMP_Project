ea67aca788552cb0d7131449bc24268140cfbc89
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-07-27 20:02:49 -0700

    Remove PHP info display from index.php
    Removed PHP info section from index.php.

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,11 +9,7 @@
   <li><a href="module_list.php">List of php modules</a></li>
   <li><a href="upload.php">Files Upload</a></li>
   <li><a href="database.php">Database Insert and Display</a></li>
+  <li><a href="phpinfo.php">PHP Info</a></li>
 </ul>
 </body>
 </html>
-
-<h3>Php info</h3>
-<?php
-phpinfo();
-?>
